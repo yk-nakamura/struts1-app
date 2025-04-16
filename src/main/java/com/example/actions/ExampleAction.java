59fd1607cb9f22e1fb988a2c6b461013294df2e2
yk-nakamura <yuki.nakamura1@jp.ricoh.com>
2025-04-16 14:49:30 +0900

    feat: DynaActionFormを使うように変更

--- src/main/java/com/example/actions/ExampleAction.java
+++ src/main/java/com/example/actions/ExampleAction.java
@@ -1,10 +1,10 @@
 package com.example.actions;
 
-import com.example.forms.ExampleForm;
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.Action;
+import org.apache.struts.action.DynaActionForm;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,12 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 public class ExampleAction extends Action {
 
     @Override
-    public ActionForward execute(ActionMapping mapping, ActionForm form,
-                                 HttpServletRequest request, HttpServletResponse response) {
-        ExampleForm exampleForm = (ExampleForm) form;
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+        DynaActionForm exampleForm = (DynaActionForm) form;
 
-        System.out.println(exampleForm.getExampleField());
-        System.out.println(exampleForm.getMessage());
+        System.out.println(exampleForm.get("exampleField").equals(""));
+        System.out.println(exampleForm.get("message").equals(""));
         
         // Forward to the success page
         return mapping.findForward("success");
